ec843de85b6ebc1ad1923bc895133d4d4e6f170b
Dima Nazarchuk <dmitry.nazarchuk.work@gmail.com>
2020-04-06 23:11:58 +0300

    Revert changes

--- src/pages/Posts/List.js
+++ src/pages/Posts/List.js
@@ -3,9 +3,6 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import actions from 'actions'
 
-// eslint-disable-next-line no-console
-console.log(process.env.REACT_APP_API_HOST)
-
 const PostsList = () => {
   const dispatch = useDispatch()
   const posts = useSelector((state) => state.posts.posts)
